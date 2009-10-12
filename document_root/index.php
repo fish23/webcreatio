77cfd16564bb792796f4f3741c545fe5b2572f74
wajrou <wajrou@gmail.com>
2009-10-12 06:45:30 +0200

    put running time to html comment at end of page
    Signed-off-by: wajrou <wajrou@gmail.com>

--- document_root/index.php
+++ document_root/index.php
@@ -1,5 +1,9 @@
 <?php
 
+// get time (with microseconds) on start of application
+list($usec, $sec) = explode(' ', microtime());
+$mtime_start = (float) $sec + (float) $usec;
+
 // absolute filesystem path to the web root
 define('WWW_DIR', dirname(__FILE__));
 
@@ -11,3 +15,7 @@ define('LIBS_DIR', WWW_DIR . '/../libs');
 
 // load bootstrap file
 require APP_DIR . '/bootstrap.php';
+
+// print running time of application (in miliseconds) within html comment
+list($usec, $sec) = explode(' ', microtime());
+echo('<!-- running time: '.round(((float) $sec + (float) $usec - $mtime_start) * 1000, 3).' ms -->');
