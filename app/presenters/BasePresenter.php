5d3121c1eea4e86806ce302c462032c05b4044ce
wajrou <wajrou@gmail.com>
2009-10-24 11:37:00 +0200

    phpdoc and comments in English
    Signed-off-by: wajrou <wajrou@gmail.com>

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -1,12 +1,19 @@
 <?php
+/**
+ * BasePresenter is parent presenter of all WebCreatio's Presenters
+ *
+ * @author Tomas Goldir <tomas.goldir@gmail.com>
+ * @copyright Copyleft (@) http://www.gnu.org/copyleft
+ * @package webcreatio
+ */
 
 abstract class BasePresenter extends Presenter
 {
-
 	protected function beforeRender()
 	{
-		// nette-0.9 registruje filtr CurlyBracketsFilter automaticky
-		//$this->template->registerFilter('CurlyBracketsFilter::invoke');	// nette-0.8 vyzaduje explicitni zapnuti CurlyBracketFilteru
+		// nette-0.9 registers CurlyBracketsFilter automaticaly
+		//$this->template->registerFilter('CurlyBracketsFilter::invoke');	// nette-0.8 requires CurlyBracketFilter explicitly
+
 		/**
 		 * @TODO setlocale('cs') or 'en' doesn't work on linux (cs_CZ works). Is it working on windows? Change language by browser settings.
 		 */
