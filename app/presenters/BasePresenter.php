c28570f1d6af6359be5e4e8b79b95d768b0178ea
wajrou <wajrou@gmail.com>
2009-10-26 18:06:51 +0100

    support for i18n
    Signed-off-by: wajrou <wajrou@gmail.com>

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -14,13 +14,7 @@ abstract class BasePresenter extends Presenter
 		// nette-0.9 registers CurlyBracketsFilter automaticaly
 		//$this->template->registerFilter('CurlyBracketsFilter::invoke');	// nette-0.8 requires CurlyBracketFilter explicitly
 
-		/**
-		 * @TODO setlocale('cs') or 'en' doesn't work on linux (cs_CZ works). Is it working on windows? Change language by browser settings.
-		 */
-		Environment::setVariable('lang', 'cs_CZ');
-
-		$translator = new Translator(
-			Environment::getVariable('lang'), APP_DIR . '/locale');
+		$translator = new Translator;
 		$this->template->setTranslator($translator);
 
 		$this->template->user = Environment::getUser();
--- app/presenters/Translator.php
+++ app/presenters/Translator.php
@@ -2,55 +2,76 @@
 
 class Translator implements ITranslator
 {
-    public $countRegexp = '#\%([0-9]+\$)*d#';
-    public $paramsRegexp = '#\%([0-9]+\$)*[fs]#';
-    
-    /**
-     * Gets the Gettext ready
-     *
-     * @param string $locale
-     * @param string $directory
-     * @param string $domain
-     */
-    public function __construct($locale, $directory, $domain = 'messages')
-    {
-        putenv("LANG=$locale");
-        setlocale(LC_ALL, $locale);
-        
-        // language pack path: $directory/$locale/LC_$domain/$domain.mo
-        // example: application/locale/cs/LC_MESSAGES/messages.mo
-        bindtextdomain($domain, $directory);
-        textdomain($domain);
-        bind_textdomain_codeset($domain, 'UTF-8');
-    }
-    
-    /**
-     * Translates the given string.
-     * @param  string   message
-     * @param  int      count
-     * @return string
-     */
-    public function translate($message, $count = NULL)
-    {
-        $pars = func_get_args();
-        
-        if (!preg_match($this->countRegexp, $message)) {                
-            $message = gettext($message);
-        } else {
-            $split = preg_split($this->countRegexp, $message);
-            $n = preg_match($this->paramsRegexp, $split[0]);
-            
-            if (!isset($pars[$n+1]))
-                throw new InvalidArgumentException('Insufficient number of arguments in translate function');
-            
-            $message = ngettext($message, $message, $pars[$n+1]);
-        }
-        
-        if (count($pars)>1) {
-            array_shift($pars);
-            return vsprintf($message, $pars); // parametrizovane preklady zatim nefunguji
-        }
+	public $defaultDirectory = '%appDir%/locale';
 
-        return $message;   
-    }   
+	public $countRegexp = '#\%([0-9]+\$)*d#';
+
+	public $paramsRegexp = '#\%([0-9]+\$)*[fs]#';
+	
+
+
+	/**
+	 * Gets the Gettext ready
+	 *
+	 * @param string $locale
+	 * @param string $directory
+	 * @param string $domain
+	 */
+	public function __construct($locale = NULL, $directory = NULL, $domain = 'messages')
+	{
+		// autodetection of language by browser settings
+		if ($locale === NULL) {
+			$langs = explode(',', Environment::getConfig('translator')->langs);
+			switch (Environment::getHttpRequest()->detectLanguage($langs)) {
+				case 'cs': $locale = 'cs_CZ'; break;
+				default:
+				case 'en': $locale = 'en_US'; break;
+			}
+			Environment::setVariable('lang', $locale);
+		}
+		putenv("LANG=$locale");
+		setlocale(LC_ALL, $locale);
+
+		// use default directory if directory wasn't specified
+		if ($directory === NULL)
+			$directory = Environment::expand($this->defaultDirectory);
+
+		// language pack path: $directory/$locale/LC_$domain/$domain.mo
+		// example: app/locale/cs/LC_MESSAGES/messages.mo
+		bindtextdomain($domain, $directory);
+		textdomain($domain);
+		bind_textdomain_codeset($domain, 'UTF-8');
+	}
+
+
+
+	/**
+	 * Translates the given string.
+	 * @param  string   message
+	 * @param  int      count
+	 * @return string
+	 */
+	public function translate($message, $count = NULL)
+	{
+		$pars = func_get_args();
+
+		if (!preg_match($this->countRegexp, $message)) {                
+			$message = gettext($message);
+		} else {
+			$split = preg_split($this->countRegexp, $message);
+			$n = preg_match($this->paramsRegexp, $split[0]);
+		  
+			if (!isset($pars[$n+1]))
+				throw new InvalidArgumentException('Insufficient number of arguments in translate function');
+		  
+			$message = ngettext($message, $message, $pars[$n+1]);
+		}
+	    
+		if (count($pars)>1) {
+			array_shift($pars);
+			return vsprintf($message, $pars); // parametrizovane preklady zatim nefunguji
+		}
+
+		return $message;   
+	}
 }
